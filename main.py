e29a90fc22cd745395916fd5212418a4985cfe46
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2026-01-04 19:26:35 +0700

    test get variable env

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
 def main():
     print("Hello from langchain!")
+    print(os.environ.get("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
