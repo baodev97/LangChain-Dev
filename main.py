8a7ab667ec8ef1789db1543295cf563cad2336fc
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2026-01-05 20:54:42 +0700

    change ChatModel from OpenAi to Ollama gemma3:270

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 def main():
@@ -20,7 +21,8 @@ Musk is a supporter of global far-right figures, causes, and political parties. 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
-    llm = ChatOpenAI(temperature=0,model="gpt-5")
+    # llm = ChatOpenAI(temperature=0,model="gpt-5")
+    llm = ChatOllama(temperature=0,model="gemma3:270m")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
     print(response.content)
